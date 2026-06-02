c214327a2632e579516094ef70fe9c3dd1153c7e
Tulbm <tuliobmartins@gmail.com>
2026-06-02 12:19:23 -0400

    lintering test files

--- tests/testthat/test-determine_effects.R
+++ tests/testthat/test-determine_effects.R
@@ -1,9 +1,9 @@
 test_that("determine_effects", {
   sim_data <- simulate_effect_data(n = 400, seed = 123)
-  true_heterogeneous <- 'x1'
-  true_homogeneous <- c('x2','x3')
-  selected_covariates <- c('x1','x2','x3')
-  formula <- make_formula(predictors = selected_covariates, response = 'y')
+  true_heterogeneous <- "x1"
+  true_homogeneous <- c("x2", "x3")
+  selected_covariates <- c("x1", "x2", "x3")
+  formula <- make_formula(predictors = selected_covariates, response = "y")
 
   model <- WMRModel$new(
     formula = formula,
@@ -13,6 +13,6 @@ test_that("determine_effects", {
     control = build_control()
   )
 
-  expect_no_error(determine_effects(model = model,direction = "forward"))
-  expect_no_error(determine_effects(model = model,direction = "backward"))
+  expect_no_error(determine_effects(model = model, direction = "forward"))
+  expect_no_error(determine_effects(model = model, direction = "backward"))
 })
