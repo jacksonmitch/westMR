163fc43ff2792dbcea1a43c41d3f557b5137dd64
Tulbm <tuliobmartins@gmail.com>
2026-06-25 18:05:17 -0400

    removed old simulate data function

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,33 +1,3 @@
-simulate_effect_data <- function(n, seed) {
-  set.seed(seed)
-  n <- n
-
-  x1 <- rnorm(n)
-  x2 <- rnorm(n)
-  x3 <- rnorm(n)
-
-  pi_true <- c(0.4, 0.6)
-  z <- sample.int(2, size = n, replace = TRUE, prob = pi_true)
-
-  beta0 <- c(-1.5, 1.5)
-  beta1 <- c(-3, 3)
-  beta2 <- 2
-  beta3 <- 0.5
-
-  sigma <- c(0.5, 0.5)
-
-  mu <- beta0[z] + beta1[z] * x1 + beta2 * x2 + beta3 * x3
-  y <- rnorm(n, mu, sigma[z])
-
-  data.frame(
-    y = y,
-    x1 = x1,
-    x2 = x2,
-    x3 = x3,
-    true_group = z
-  )
-}
-
 simulate_fmr <- function(n, betas, pi = NULL, sigma = 1, seed = NULL) {
   if (!is.null(seed)) set.seed(seed)
 
--- tests/testthat/test-determine_effects.R
+++ tests/testthat/test-determine_effects.R
@@ -1,29 +1,19 @@
 test_that("determine_effects", {
-  sim_data <- simulate_effect_data(n = 400, seed = 123)
-  true_heterogeneous <- "x1"
-  true_homogeneous <- c("x2", "x3")
-  selected_covariates <- c("x1", "x2", "x3")
-  formula <- make_formula(predictors = selected_covariates, response = "y")
+  test_data <- do.call(
+    simulate_fmr,
+    c(
+      scenarios$two_group_effects,
+      list(n = 400, seed = 123)
+    )
+  )
 
   model <- WMRModel$new(
-    formula = formula,
-    data = sim_data,
-    G_values = 2:3,
+    formula = formula(test_data),
+    data = test_data,
+    G_values = 2,
     family = "gaussian",
     control = build_control()
   )
-
-  # test_data <- do.call(simulate_fmr,
-  #                      c(scenarios$two_group_effects,
-  #                        list(n = 400, seed = 123)))
-  #
-  # model <- WMRModel$new(
-  #   formula = formula(test_data),
-  #   data = test_data,
-  #   G_values = 2,
-  #   family = "gaussian",
-  #   control = build_control()
-  # )
 
   expect_no_error(determine_effects(model = model, direction = "forward"))
   expect_no_error(determine_effects(model = model, direction = "backward"))
